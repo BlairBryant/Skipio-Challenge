07d05af0e9141a4509c5c7e2679bf049220b8707
Blair <blairslc@gmail.com>
2018-10-22 19:44:10 -0600

    Finished Forms

--- src/answers/Forms/index.js
+++ src/answers/Forms/index.js
@@ -10,40 +10,48 @@ class LoginForm extends React.Component {
     password: ''
   };
 
+  emailInput = e => {
+    this.setState({email: e.target.value})
+  }
+
+  passwordInput = e => {
+    this.setState({password: e.target.value})
+  }
+
+  submitForm = async () => {
+    this.setState({ user: await login(this.state.email, this.state.password)});
+  }
+
   render() {
-    const doit = async () => {
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      console.log(email, password)
-      this.setState({ user: await login(email, password) });
-    };
-
-    if (this.state.user) return <div>Welcome {this.state.user.name}</div>;
-
     return (
-      <div>
-        Open <code>/src/answers/Forms/index.js</code> to start
-        <br />
-        <br />
-        Email: <input type="email" id="email" />
-        <br />
-        <br />
-        Password: <input type="password" id="password" />
-        <br />
-        <br />
-        <span
-          style={{
-            background: "#eee",
-            border: "1px solid #ccc",
-            display: "inline-block",
-            padding: "5px 20px"
-          }}
-          onClick={doit}
-        >
-          Log In
-        </span>
-      </div>
-    );
+      this.state.user
+      ?
+      <div>Welcome {this.state.user.name}</div>
+      :
+      <form onSubmit={this.submitForm}>
+        <div>
+          Open <code>/src/answers/Forms/index.js</code> to start
+          <br />
+          <br />
+          Email: <input type="email" id="email" onChange={this.emailInput}/>
+          <br />
+          <br />
+          Password: <input type="password" id="password" onChange={this.passwordInput}/>
+          <br />
+          <br />
+          <button
+            style={{
+              background: "#eee",
+              border: "1px solid #ccc",
+              display: "inline-block",
+              padding: "5px 20px"
+            }}
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+    )
   }
 }
 
